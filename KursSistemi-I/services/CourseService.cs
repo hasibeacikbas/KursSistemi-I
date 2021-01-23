d705fde69c4a6188df9bb17023df4560a7ee4bce
Hasibe Açıkbaş <74353520+hasibeacikbas@users.noreply.github.com>
2021-01-24 01:30:18 +0300

    CourseService eklendi.

---
+++ KursSistemi-I/services/CourseService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursSistemi_I.services
+{
+    class CourseService
+    {
+        CourseOtomationEntities2 courseOtomationEntitiesCourse;
+        public CourseService()
+        {
+            courseOtomationEntitiesCourse = new CourseOtomationEntities2();
+        }
+        public Course getCourse(int id)
+        {
+            var x = courseOtomationEntitiesCourse.Course.Find(id);
+            return x;
+        }
+
+        public List<Course> getCourses()
+        {
+
+            return courseOtomationEntitiesCourse.Course.ToList();
+        }
+        public Course addCourse(Course course)
+        {
+            courseOtomationEntitiesCourse.Course.Add(course);
+            courseOtomationEntitiesCourse.SaveChanges();
+            return course;
+        }
+    }
+}
